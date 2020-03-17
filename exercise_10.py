f05ac3c40fa556e8884bba4f13a89d60aa9bced7
Gabriel <ionescuig@yahoo.com>
2020-03-17 19:18:52 +0000

    Add files via upload

---
+++ exercise_10.py
@@ -0,0 +1,32 @@
+#!/usr/bin python3
+# title           :exercise 10
+# description     :Exercises from https://www.practicepython.org
+# author          :Gabriel Ionescu
+# date            :2020/03/17
+# version         :1.0
+# notes           :
+# python_version  :3.7.5
+# ==============================================================================
+
+"""
+Exercise 10:
+Take two lists, say for example these two:
+	a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+	b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+and write a program that returns a list that contains only the elements
+that are common between the lists (without duplicates).
+Make sure your program works on two lists of different sizes.
+Write this using at least one list comprehension.
+(Hint: Remember list comprehensions from Exercise 7).
+"""
+
+from random import randint
+print('\n=====')
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+c = {el for el in a if el in b}
+
+print(list(c))
+
+print('=====\n')
